8d0a856847208ac6662b996ce5d4a1f359238b22
Tyler Machen <tylermachen@gmail.com>
2015-08-04 14:35:22 -0400

    complete Book class

---
+++ lib/book.rb
@@ -0,0 +1,21 @@
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+Book.new("And Then There Were None")
