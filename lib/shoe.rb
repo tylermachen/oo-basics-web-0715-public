b81c0aa96f5ce4a8fb87f2c8b8b2ef2a436cb920
Tyler Machen <tylermachen@gmail.com>
2015-08-04 14:41:08 -0400

    complete Shoe class

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,15 @@
 class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  BRANDS = []
+
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand unless BRANDS.include?(brand)
+  end
+
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
+  end
 end
